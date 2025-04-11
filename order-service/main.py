a806196d36b3c591897f52b8a499bd4523f90b5c
Mr.Nayra <aryanedu5786@gmail.com>
2025-04-11 10:20:23 +0530

    Cors for order api

--- order-service/main.py
+++ order-service/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from app.routers import orders
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Include routers
 app.include_router(orders.router)
